87e3da13918f4ce4c1a81394d464bd5be07761a2
Alastair Reid <adreid@google.com>
2021-03-01 19:00:46 +0000

    Annotations to support splits and merges
    This adds several ways of performing case splits that may
    be exhaustive or non-exhaustive.
    
    And it adds the coherent! block macro that merges paths created
    within the block.

--- verification-annotations/src/klee.rs
+++ verification-annotations/src/klee.rs
@@ -21,7 +21,12 @@ extern "C" {
     fn klee_assume(cond: usize);
     fn klee_abort() -> !;
     fn klee_silent_exit(_ignored: u32) -> !;
-    fn klee_is_replay() -> i32;
+    fn klee_is_replay() -> u32;
+    fn klee_choose(x: usize) -> usize;
+    fn klee_get_value_i32(x: i32) -> i32;
+    fn klee_get_value_i64(x: i64) -> i64;
+    fn klee_open_merge();
+    fn klee_close_merge();
 }
 
 /// Create instance for any type consisting of contiguous memory
@@ -114,6 +119,69 @@ pub fn is_replay() -> bool {
     unsafe { klee_is_replay() != 0 }
 }
 
+pub fn get_value_i32(x: i32) -> i32 {
+    unsafe { klee_get_value_i32(x) }
+}
+
+pub fn get_value_i64(x: i64) -> i64 {
+    unsafe { klee_get_value_i64(x) }
+}
+
+pub fn get_value_u32(x: u32) -> u32 {
+    get_value_i32(x as i32) as u32
+}
+
+pub fn get_value_u64(x: u64) -> u64 {
+    get_value_i64(x as i64) as u64
+}
+
+/// Open a merge block
+///
+/// Should be paired with `close_merge`
+pub fn open_merge() {
+    // safe because it only affects KLEE scheduling
+    unsafe {
+        klee_open_merge();
+    }
+}
+
+/// Close a merge block
+///
+/// Should be paired with `open_merge`
+pub fn close_merge() {
+    // safe because it only affects KLEE scheduling
+    unsafe {
+        klee_close_merge();
+    }
+}
+
+/// Coherent blocks don't fork execution during verification.
+///
+/// This will only take effect if executed with the
+/// KLEE command line flags `--use-merge` and, optionally,
+/// `--use-incomplete-merge`.
+///
+/// This might reduce the number of instructions that KLEE explores
+/// because there are less forks.
+/// This might also make evaluation of the symbolic constraints
+/// more expensive because of state merging.
+///
+/// Caveats:
+/// - Branches out of the middle of `$body` such as return, etc. will not be merged.
+///   If this is a problem, you should use open_merge/close_merge explicitly.
+///
+/// - If the body performs memory allocation, merging cannot happen.
+#[macro_export]
+macro_rules! coherent {
+    ( $($body:stmt;)+ ) => {
+        $crate::open_merge();
+        {
+            $($body;)+
+        }
+        $crate::close_merge();
+    };
+}
+
 /// Reject the current execution with a verification failure
 /// and an error message.
 pub fn report_error(message: &str) -> ! {
@@ -122,6 +190,78 @@ pub fn report_error(message: &str) -> ! {
     // supported by the kleeRuntest library.)
     eprintln!("KLEE: ERROR:{}", message);
     abort();
+}
+
+/// Split exploration into two cases: one satisfying `x`, one not.
+///
+/// This is one way of forcing KLEE to make a case split and
+/// concretize a choice.
+pub fn case_split(x: bool) {
+    if x {
+        // dummy call to prevent compiler from deleting this call
+        crate::verifier_use_u32(0)
+    }
+}
+
+/// Split exploration into one case for each value in xs.
+///
+/// This is one way of forcing KLEE to make a case split and
+/// concretize a choice.
+pub fn any_of<T>(xs: &[T]) -> &T {
+    let n = xs.len();
+    if n == 0 {
+        report_error("verifier::choose() empty choice")
+    }
+    let c = unsafe { klee_choose(n) };
+    for (i, x) in xs.iter().enumerate() {
+        if i == c {
+            return x;
+        }
+    }
+    report_error("verifier::choose(): internal error: failed to choose value")
+}
+
+/// Split exploration into `samples` cases by choosing a distinct concrete solution
+/// for `x` in each case.
+///
+/// In most cases, this results in an incomplete exploration because there may
+/// be more possible solutions than we explore.
+pub fn sample_u32(samples: usize, x: u32) -> u32 {
+    for _i in 0..samples-1 {
+        let s = get_value_u32(x);
+        if s == x {
+            return s;
+        }
+    }
+    get_value_u32(x)
+}
+
+// todo: this should be replaced with a universal hash function
+// (https://en.wikipedia.org/wiki/Universal_hashing)
+fn hash(x: u32) -> u32 {
+    u32::wrapping_add(u32::wrapping_mul(x, 1664525), 1013904223)
+}
+
+// In theory, this should give more random sampling than
+// `sample_u32` but, in practice, KLEE's solver very, very
+// strongly prefers to find the smallest values so
+// all this does is randomize the order in which values are generated.
+//
+// If we want actually random values, then maybe we need
+// to generate a series of random values that we want the bottom
+// N bits of the hash function to evaluate to?
+pub fn random_sample_u32(log_samples: usize, x: u32) -> u32 {
+    let samples = 1 << log_samples;
+    let h = hash(x) & (samples - 1);
+    for i in 0..samples {
+        if i == h {
+            let s = get_value_u32(x);
+            assume(s == x);
+            return s;
+        }
+    }
+    // todo: could also report an error here since one must have matched
+    reject()
 }
 
 /// Declare that failure is the expected behaviour
