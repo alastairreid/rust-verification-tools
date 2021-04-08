a73ffe48bbaf8ff82cf1a3bb334e7420dca25011
Alastair Reid <adreid@google.com>
2021-04-08 11:41:13 +0000

    refactor code a little

--- runtime/src/x86intrinsics.rs
+++ runtime/src/x86intrinsics.rs
@@ -36,45 +36,52 @@ pub struct u8x16(u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8)
 #[inline]
 #[no_mangle]
 pub fn llvm_x86_sse2_pcmpeqb_epi8(a: u8x16, b: u8x16) -> u8x16 {
-    let r0  = if a.0  == b.0  { 0xff } else { 0x0 };
-    let r1  = if a.1  == b.1  { 0xff } else { 0x0 };
-    let r2  = if a.2  == b.2  { 0xff } else { 0x0 };
-    let r3  = if a.3  == b.3  { 0xff } else { 0x0 };
-    let r4  = if a.4  == b.4  { 0xff } else { 0x0 };
-    let r5  = if a.5  == b.5  { 0xff } else { 0x0 };
-    let r6  = if a.6  == b.6  { 0xff } else { 0x0 };
-    let r7  = if a.7  == b.7  { 0xff } else { 0x0 };
-    let r8  = if a.8  == b.8  { 0xff } else { 0x0 };
-    let r9  = if a.9  == b.9  { 0xff } else { 0x0 };
-    let r10 = if a.10 == b.10 { 0xff } else { 0x0 };
-    let r11 = if a.11 == b.11 { 0xff } else { 0x0 };
-    let r12 = if a.12 == b.12 { 0xff } else { 0x0 };
-    let r13 = if a.13 == b.13 { 0xff } else { 0x0 };
-    let r14 = if a.14 == b.14 { 0xff } else { 0x0 };
-    let r15 = if a.15 == b.15 { 0xff } else { 0x0 };
+    fn op(x: u8, y: u8) -> u8 {
+        if x == y { 0xff } else { 0x0 }
+    }
+    let r0  = op(a.0,  b.0);
+    let r1  = op(a.1,  b.1);
+    let r2  = op(a.2,  b.2);
+    let r3  = op(a.3,  b.3);
+    let r4  = op(a.4,  b.4);
+    let r5  = op(a.5,  b.5);
+    let r6  = op(a.6,  b.6);
+    let r7  = op(a.7,  b.7);
+    let r8  = op(a.8,  b.8);
+    let r9  = op(a.9,  b.9);
+    let r10 = op(a.10, b.10);
+    let r11 = op(a.11, b.11);
+    let r12 = op(a.12, b.12);
+    let r13 = op(a.13, b.13);
+    let r14 = op(a.14, b.14);
+    let r15 = op(a.15, b.15);
     u8x16(r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15)
 }
 
 #[inline]
 #[no_mangle]
 pub fn llvm_x86_sse2_pcmpeqw_epi16(a: u16x8, b: u16x8) -> u16x8 {
-    let r0 = if a.0  == b.0 { 0xff } else { 0x0 };
-    let r1 = if a.1  == b.1 { 0xff } else { 0x0 };
-    let r2 = if a.2  == b.2 { 0xff } else { 0x0 };
-    let r3 = if a.3  == b.3 { 0xff } else { 0x0 };
-    let r4 = if a.4  == b.4 { 0xff } else { 0x0 };
-    let r5 = if a.5  == b.5 { 0xff } else { 0x0 };
-    let r6 = if a.6  == b.6 { 0xff } else { 0x0 };
-    let r7 = if a.7  == b.7 { 0xff } else { 0x0 };
+    fn op(x: u16, y: u16) -> u16 {
+        if x == y { 0xffff } else { 0x0 }
+    }
+    let r0 = op(a.0, b.0);
+    let r1 = op(a.1, b.1);
+    let r2 = op(a.2, b.2);
+    let r3 = op(a.3, b.3);
+    let r4 = op(a.4, b.4);
+    let r5 = op(a.5, b.5);
+    let r6 = op(a.6, b.6);
+    let r7 = op(a.7, b.7);
     u16x8(r0, r1, r2, r3, r4, r5, r6, r7)
 }
 
 
 #[inline]
+#[target_feature(enable = "sse2")]
 #[no_mangle]
-pub fn llvm_x86_sse2_psrli_w(a: u16x8, imm8: i32) -> u16x8 {
-    let imm8: i32 = imm8 & 0xff;
-    fn op(x: u16, imm8: i32) -> u16 {
+pub unsafe fn llvm_x86_sse2_psrli_w(a: u16x8, imm8: i32) -> u16x8 {
+    let imm8 = imm8 as u8;
+    fn op(x: u16, imm8: u8) -> u16 {
         if imm8 > 15 { 0 } else { x >> imm8 }
     }
     let r0  = op(a.0, imm8);
@@ -91,29 +98,29 @@ pub fn llvm_x86_sse2_psrli_w(a: u16x8, imm8: i32) -> u16x8 {
 
 #[inline]
 #[no_mangle]
-pub fn llvm_x86_sse2_pmovmskb_128(x: u8x16) -> i32 {
-    let r0  = ((x.0 >> 7) & 1) as i32;
-    let r1  = ((x.1 >> 7) & 1) as i32;
-    let r2  = ((x.2 >> 7) & 1) as i32;
-    let r3  = ((x.3 >> 7) & 1) as i32;
-    let r4  = ((x.4 >> 7) & 1) as i32;
-    let r5  = ((x.5 >> 7) & 1) as i32;
-    let r6  = ((x.6 >> 7) & 1) as i32;
-    let r7  = ((x.7 >> 7) & 1) as i32;
-    let r8  = ((x.8 >> 7) & 1) as i32;
-    let r9  = ((x.9 >> 7) & 1) as i32;
-    let r10 = ((x.10 >> 7) & 1) as i32;
-    let r11 = ((x.11 >> 7) & 1) as i32;
-    let r12 = ((x.12 >> 7) & 1) as i32;
-    let r13 = ((x.13 >> 7) & 1) as i32;
-    let r14 = ((x.14 >> 7) & 1) as i32;
-    let r15 = ((x.15 >> 7) & 1) as i32;
+pub fn llvm_x86_sse2_pmovmskb_128(a: u8x16) -> i32 {
+    fn op(x: u8) -> i32 {
+        ((x >> 7) & 1) as i32
+    }
+    let r0  = op(a.0);
+    let r1  = op(a.1);
+    let r2  = op(a.2);
+    let r3  = op(a.3);
+    let r4  = op(a.4);
+    let r5  = op(a.5);
+    let r6  = op(a.6);
+    let r7  = op(a.7);
+    let r8  = op(a.8);
+    let r9  = op(a.9);
+    let r10 = op(a.10);
+    let r11 = op(a.11);
+    let r12 = op(a.12);
+    let r13 = op(a.13);
+    let r14 = op(a.14);
+    let r15 = op(a.15);
     let r = (r0  << 0)  | (r1  << 1)  | (r2  << 2)  | (r3  << 3)
           | (r4  << 4)  | (r5  << 5)  | (r6  << 6)  | (r7  << 7)
           | (r8  << 8)  | (r9  << 9)  | (r10 << 10) | (r11 << 11)
           | (r12 << 12) | (r13 << 13) | (r14 << 14) | (r15 << 15);
     r
 }
-
-
-
