e29d868cf6f0379b5666c509f94445f2240a9892
Alastair Reid <adreid@google.com>
2021-04-09 14:21:32 +0000

    Switch to vectorcall calling convention

--- runtime/src/x86intrinsics.rs
+++ runtime/src/x86intrinsics.rs
@@ -7,6 +7,7 @@
 // except according to those terms.
 
 #![feature(repr_simd)]
+#![feature(abi_vectorcall)]
 
 #[derive(Copy, Clone, Debug)]
 #[allow(non_camel_case_types)]
@@ -35,7 +36,35 @@ pub struct u8x16(u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8)
 
 #[inline]
 #[no_mangle]
-pub fn llvm_x86_sse2_pcmpeqb_epi8(a: u8x16, b: u8x16) -> u8x16 {
+extern "vectorcall" fn veccall_test(a: __m128i, b: __m128i) -> __m128i {
+    fn op(x: u8, y: u8) -> u8 {
+        if x == y { 0xff } else { 0x0 }
+    }
+    let a = unsafe { std::mem::transmute::<__m128i, u8x16>(a) };
+    let b = unsafe { std::mem::transmute::<__m128i, u8x16>(b) };
+    let r0  = op(a.0,  b.0);
+    let r1  = op(a.1,  b.1);
+    let r2  = op(a.2,  b.2);
+    let r3  = op(a.3,  b.3);
+    let r4  = op(a.4,  b.4);
+    let r5  = op(a.5,  b.5);
+    let r6  = op(a.6,  b.6);
+    let r7  = op(a.7,  b.7);
+    let r8  = op(a.8,  b.8);
+    let r9  = op(a.9,  b.9);
+    let r10 = op(a.10, b.10);
+    let r11 = op(a.11, b.11);
+    let r12 = op(a.12, b.12);
+    let r13 = op(a.13, b.13);
+    let r14 = op(a.14, b.14);
+    let r15 = op(a.15, b.15);
+    let r = u8x16(r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15);
+    unsafe { std::mem::transmute::<u8x16, __m128i>(r) }
+}
+
+#[inline]
+#[no_mangle]
+extern "vectorcall" fn llvm_x86_sse2_pcmpeqb_epi8(a: u8x16, b: u8x16) -> u8x16 {
     fn op(x: u8, y: u8) -> u8 {
         if x == y { 0xff } else { 0x0 }
     }
@@ -60,7 +89,7 @@ pub fn llvm_x86_sse2_pcmpeqb_epi8(a: u8x16, b: u8x16) -> u8x16 {
 
 #[inline]
 #[no_mangle]
-pub fn llvm_x86_sse2_pcmpeqw_epi16(a: u16x8, b: u16x8) -> u16x8 {
+extern "vectorcall" fn llvm_x86_sse2_pcmpeqw_epi16(a: u16x8, b: u16x8) -> u16x8 {
     fn op(x: u16, y: u16) -> u16 {
         if x == y { 0xffff } else { 0x0 }
     }
@@ -75,11 +104,9 @@ pub fn llvm_x86_sse2_pcmpeqw_epi16(a: u16x8, b: u16x8) -> u16x8 {
     u16x8(r0, r1, r2, r3, r4, r5, r6, r7)
 }
 
-
 #[inline]
-#[target_feature(enable = "sse2")]
 #[no_mangle]
-pub unsafe fn llvm_x86_sse2_psrli_w(a: u16x8, imm8: i32) -> u16x8 {
+extern "vectorcall" fn llvm_x86_sse2_psrli_w(a: u16x8, imm8: i32) -> u16x8 {
     let imm8 = imm8 as u8;
     fn op(x: u16, imm8: u8) -> u16 {
         if imm8 > 15 { 0 } else { x >> imm8 }
@@ -98,7 +125,7 @@ pub unsafe fn llvm_x86_sse2_psrli_w(a: u16x8, imm8: i32) -> u16x8 {
 
 #[inline]
 #[no_mangle]
-pub fn llvm_x86_sse2_pmovmskb_128(a: u8x16) -> i32 {
+extern "vectorcall" fn llvm_x86_sse2_pmovmskb_128(a: u8x16) -> i32 {
     fn op(x: u8) -> i32 {
         ((x >> 7) & 1) as i32
     }
