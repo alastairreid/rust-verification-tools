5ef2ca9fc996b676e39d1b76d074d4f94c6ab955
Alastair Reid <adreid@google.com>
2021-04-11 09:17:18 +0000

    simd: add Makefile

--- cargo-verify/src/main.rs
+++ cargo-verify/src/main.rs
@@ -565,12 +565,15 @@ fn build(opt: &Opt, package: &str, target: &str) -> CVResult<PathBuf> {
     let backend_lc = format!("{}", opt.backend).to_lowercase();
     let runtime = format!("{}/runtime/rvt-{}.bc", rvt_dir, backend_lc);
     let runtime = PathBuf::from(&runtime);
+    let simd_intrinsics = format!("{}/simd_intrinsics/simd_intrinsics.bc", rvt_dir);
+    let simd_intrinsics = PathBuf::from(&simd_intrinsics);
     info_at!(
         &opt,
         Verbosity::Minor,
-        "  Linking {}, {} and [{}] to produce {}",
+        "  Linking {}, {}, {} and [{}] to produce {}",
         bc_file.to_string_lossy(),
         runtime.to_string_lossy(),
+        simd_intrinsics.to_string_lossy(),
         c_files
             .iter()
             .map(|p| p.to_string_lossy())
@@ -583,6 +586,7 @@ fn build(opt: &Opt, package: &str, target: &str) -> CVResult<PathBuf> {
         .arg("-o")
         .arg(&new_bc_file)
         .arg(runtime)
+        .arg(simd_intrinsics)
         .arg(&bc_file)
         .args(&c_files)
         .latin1_output_info(&opt, Verbosity::Major)?;
