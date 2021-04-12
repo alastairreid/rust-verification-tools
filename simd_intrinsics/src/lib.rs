b0fcdf06a23707c4f40a4fe127d3ba46ca8bf971
Alastair Reid <adreid@google.com>
2021-04-12 11:12:24 +0000

    Start refactoring simd ops

--- simd_intrinsics/src/lib.rs
+++ simd_intrinsics/src/lib.rs
@@ -33,14 +33,23 @@ pub struct u16x8(u16, u16, u16, u16, u16, u16, u16, u16);
 #[repr(simd)]
 pub struct u8x16(u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8);
 
-#[inline]
-#[no_mangle]
-extern "C" fn veccall_test(a: __m128i, b: __m128i) -> __m128i {
-    fn op(x: u8, y: u8) -> u8 {
-        if x == y { 0xff } else { 0x0 }
-    }
-    let a = unsafe { std::mem::transmute::<__m128i, u8x16>(a) };
-    let b = unsafe { std::mem::transmute::<__m128i, u8x16>(b) };
+fn map8_vs_v<F>(op: F, a: u16x8, b: u16) -> u16x8
+    where F: Fn(u16, u16) -> u16
+{
+    let r0  = op(a.0,  b);
+    let r1  = op(a.1,  b);
+    let r2  = op(a.2,  b);
+    let r3  = op(a.3,  b);
+    let r4  = op(a.4,  b);
+    let r5  = op(a.5,  b);
+    let r6  = op(a.6,  b);
+    let r7  = op(a.7,  b);
+    u16x8(r0, r1, r2, r3, r4, r5, r6, r7)
+}
+
+fn map8_vv_v<F>(op: F, a: u16x8, b: u16x8) -> u16x8
+    where F: Fn(u16, u16) -> u16
+{
     let r0  = op(a.0,  b.0);
     let r1  = op(a.1,  b.1);
     let r2  = op(a.2,  b.2);
@@ -49,24 +58,12 @@ extern "C" fn veccall_test(a: __m128i, b: __m128i) -> __m128i {
     let r5  = op(a.5,  b.5);
     let r6  = op(a.6,  b.6);
     let r7  = op(a.7,  b.7);
-    let r8  = op(a.8,  b.8);
-    let r9  = op(a.9,  b.9);
-    let r10 = op(a.10, b.10);
-    let r11 = op(a.11, b.11);
-    let r12 = op(a.12, b.12);
-    let r13 = op(a.13, b.13);
-    let r14 = op(a.14, b.14);
-    let r15 = op(a.15, b.15);
-    let r = u8x16(r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15);
-    unsafe { std::mem::transmute::<u8x16, __m128i>(r) }
+    u16x8(r0, r1, r2, r3, r4, r5, r6, r7)
 }
 
-#[inline]
-#[no_mangle]
-extern "C" fn llvm_x86_sse2_pcmpeqb_epi8(a: u8x16, b: u8x16) -> u8x16 {
-    fn op(x: u8, y: u8) -> u8 {
-        if x == y { 0xff } else { 0x0 }
-    }
+fn map16_vv_v<F>(op: F, a: u8x16, b: u8x16) -> u8x16
+    where F: Fn(u8, u8) -> u8
+{
     let r0  = op(a.0,  b.0);
     let r1  = op(a.1,  b.1);
     let r2  = op(a.2,  b.2);
@@ -86,21 +83,35 @@ extern "C" fn llvm_x86_sse2_pcmpeqb_epi8(a: u8x16, b: u8x16) -> u8x16 {
     u8x16(r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15)
 }
 
+
+#[inline]
+#[no_mangle]
+extern "C" fn veccall_test(a: __m128i, b: __m128i) -> __m128i {
+    fn op(x: u8, y: u8) -> u8 {
+        if x == y { 0xff } else { 0x0 }
+    }
+    let a = unsafe { std::mem::transmute::<__m128i, u8x16>(a) };
+    let b = unsafe { std::mem::transmute::<__m128i, u8x16>(b) };
+    let r = map16_vv_v(op, a, b);
+    unsafe { std::mem::transmute::<u8x16, __m128i>(r) }
+}
+
+#[inline]
+#[no_mangle]
+extern "C" fn llvm_x86_sse2_pcmpeqb_epi8(a: u8x16, b: u8x16) -> u8x16 {
+    fn op(x: u8, y: u8) -> u8 {
+        if x == y { 0xff } else { 0x0 }
+    }
+    map16_vv_v(op, a, b)
+}
+
 #[inline]
 #[no_mangle]
 extern "C" fn llvm_x86_sse2_pcmpeqw_epi16(a: u16x8, b: u16x8) -> u16x8 {
     fn op(x: u16, y: u16) -> u16 {
         if x == y { 0xffff } else { 0x0 }
     }
-    let r0 = op(a.0, b.0);
-    let r1 = op(a.1, b.1);
-    let r2 = op(a.2, b.2);
-    let r3 = op(a.3, b.3);
-    let r4 = op(a.4, b.4);
-    let r5 = op(a.5, b.5);
-    let r6 = op(a.6, b.6);
-    let r7 = op(a.7, b.7);
-    u16x8(r0, r1, r2, r3, r4, r5, r6, r7)
+    map8_vv_v(op, a, b)
 }
 
 #[inline]
